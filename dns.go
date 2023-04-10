6676c1d9e80fdbca63e18179b0f7092007f152e1
gsmini <gsmini@sina.cn>
2023-04-11 00:18:34 +0800

    feat: debug success with replace mod in local

---
+++ dns.go
@@ -0,0 +1,25 @@
+package k8s_headless_svc
+
+import (
+	"fmt"
+	"net"
+)
+
+//dns for headless service in k8s: (service_name).$(k8s_namespace).svc.cluster.local
+func getDnsForPodIP(svc []*Service) (ipMaps map[string][]string, err error) {
+	for _, value := range svc {
+		dnsForK8sSvc := fmt.Sprintf("%s.%s.svc.svc.cluster.local", value.SvcName, value.Namespace)
+		ipRecords, err := net.LookupIP(dnsForK8sSvc)
+		if err != nil {
+			return nil, err
+		}
+		for _, ip := range ipRecords {
+			//{
+			// "user-svc":["127.0.0.1:8080","127.0.0.1:8081"]
+			//}
+			ipMaps[value.SvcName] = append(ipMaps[value.SvcName], fmt.Sprintf("%s:%d", ip.String(), value.PodPort))
+		}
+	}
+
+	return ipMaps, err
+}
---
+++ k8s-headless-svc.go
@@ -0,0 +1,102 @@
+package k8s_headless_svc
+
+import (
+	"go-micro.dev/v4/registry"
+)
+
+// about services within the registry.
+type k8sSvcWatcher struct {
+}
+
+func (k *k8sSvcWatcher) Next() (*registry.Result, error) {
+
+	return &registry.Result{}, nil
+}
+func (k *k8sSvcWatcher) Stop() {
+
+}
+
+type Service struct {
+	Namespace string //k8s namespace
+	SvcName   string //service 的name
+	PodPort   int32  //service 对应的endpoint 的port 也就是pod的container port
+}
+type k8sSvcRegister struct {
+	k8sService []*Service //当前服务可能需要依赖多个其他服务
+
+	opts *registry.Options
+}
+
+func (k *k8sSvcRegister) Init(opts ...registry.Option) error {
+	for _, o := range opts {
+		o(k.opts)
+	}
+	return nil
+}
+func (k *k8sSvcRegister) Options() registry.Options {
+	return registry.Options{}
+}
+func (k *k8sSvcRegister) Register(*registry.Service, ...registry.RegisterOption) error {
+	//解析dns 返回pod id
+	return nil
+}
+func (k *k8sSvcRegister) Deregister(*registry.Service, ...registry.DeregisterOption) error {
+	//解析dns 返回pod id
+
+	return nil
+}
+func (k *k8sSvcRegister) GetService(string, ...registry.GetOption) ([]*registry.Service, error) {
+
+	var service []*registry.Service
+	var nodes []*registry.Node
+	//
+	ipMaps, err := getDnsForPodIP(k.k8sService)
+	if err != nil {
+		return []*registry.Service{}, err
+	}
+	for svcName, ips := range ipMaps {
+		for _, ip := range ips {
+			nodes = append(nodes, &registry.Node{Address: ip})
+
+		}
+		service = append(service, &registry.Service{Name: svcName, Version: "latest", Nodes: nodes})
+
+	}
+
+	return service, nil
+}
+func (k *k8sSvcRegister) ListServices(...registry.ListOption) ([]*registry.Service, error) {
+
+	var service []*registry.Service
+	var nodes []*registry.Node
+	//
+	ipMaps, err := getDnsForPodIP(k.k8sService)
+	if err != nil {
+		return []*registry.Service{}, err
+	}
+	for svcName, ips := range ipMaps {
+		for _, ip := range ips {
+			nodes = append(nodes, &registry.Node{Address: ip})
+
+		}
+		service = append(service, &registry.Service{Name: svcName, Version: "latest", Nodes: nodes})
+
+	}
+	return service, nil
+}
+
+func (k *k8sSvcRegister) Watch(option ...registry.WatchOption) (registry.Watcher, error) {
+	return &k8sSvcWatcher{}, nil
+}
+func (k *k8sSvcRegister) String() string {
+	return "k8s_headless_svc"
+}
+
+// NewRegistry creates a kubernetes registry.
+func NewRegistry(k8sService []*Service, opts ...registry.Option) registry.Registry {
+	k := k8sSvcRegister{
+		k8sService: k8sService,
+		opts:       &registry.Options{},
+	}
+	return &k
+}
