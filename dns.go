0063c2f63fa155115b6768074fb0e4f8b67d253b
gsmini <gsmini@sina.cn>
2023-04-11 21:46:03 +0800

    doc: 修改注释

--- dns.go
+++ dns.go
@@ -6,6 +6,9 @@ import (
 )
 
 //dns for headless service in k8s: (service_name).$(k8s_namespace).svc.cluster.local
+//ipMaps data like this: { "user-svc":["127.0.0.1:8080","127.0.0.1:8081"] }
+//
+//
 func getDnsForPodIP(svc []*Service) (ipMaps map[string][]string, err error) {
 	ipMaps = make(map[string][]string, 10)
 	for _, value := range svc {
@@ -16,10 +19,7 @@ func getDnsForPodIP(svc []*Service) (ipMaps map[string][]string, err error) {
 			return nil, err
 		}
 		for _, ip := range ipRecords {
-			//{
-			// "user-svc":["127.0.0.1:8080","127.0.0.1:8081"]
-			//}
-			fmt.Println(value.SvcName, ip.String(), value.PodPort)
+
 			ipMaps[value.SvcName] = append(ipMaps[value.SvcName], fmt.Sprintf("%s:%d", ip.String(), value.PodPort))
 		}
 	}
--- k8s-headless-svc.go
+++ k8s-headless-svc.go
@@ -1,7 +1,6 @@
 package k8s_headless_svc
 
 import (
-	"fmt"
 	"go-micro.dev/v4/registry"
 )
 
@@ -23,7 +22,7 @@ type Service struct {
 	PodPort   int32  //service 对应的endpoint 的port 也就是pod的container port
 }
 type k8sSvcRegister struct {
-	k8sService []*Service //当前服务可能需要依赖多个其他服务
+	k8sService []*Service //当前服务可能需要依赖多个其他go-micro
 
 	opts *registry.Options
 }
@@ -38,21 +37,19 @@ func (k *k8sSvcRegister) Options() registry.Options {
 	return registry.Options{}
 }
 func (k *k8sSvcRegister) Register(*registry.Service, ...registry.RegisterOption) error {
-	//解析dns 返回pod id
-	// because we use k8s svc replace Register func ,so this func do nothing
+	// 解析dns 返回pod id
+	// 因为我们打算用k8s service的服务注册自己发现endpoints，所以我们自己并不需要写注册发现逻辑
 	return nil
 }
 func (k *k8sSvcRegister) Deregister(*registry.Service, ...registry.DeregisterOption) error {
-	//解析dns 返回pod id
-	// because we use k8s svc replace Register func ,so this func do nothing
-
+	// 解析dns 返回pod id
+	// 因为我们打算用k8s service的服务注册自己发现endpoints，所以我们自己并不需要写注册发现逻辑
 	return nil
 }
 func (k *k8sSvcRegister) GetService(string, ...registry.GetOption) ([]*registry.Service, error) {
 
 	var service []*registry.Service
 	var nodes []*registry.Node
-	//
 	ipMaps, err := getDnsForPodIP(k.k8sService)
 	if err != nil {
 		return []*registry.Service{}, err
@@ -60,20 +57,12 @@ func (k *k8sSvcRegister) GetService(string, ...registry.GetOption) ([]*registry.
 	for svcName, ips := range ipMaps {
 		for _, ip := range ips {
 			nodes = append(nodes, &registry.Node{Address: ip})
-
 		}
 		service = append(service, &registry.Service{Name: svcName, Version: "latest", Nodes: nodes})
-
 	}
 
 	//nodes = append(nodes, &registry.Node{Address: "127.0.0.1:8080"})
 	//service = append(service, &registry.Service{Name: "user", Version: "latest", Nodes: nodes})
-	fmt.Println("1111111")
-	fmt.Println(service)
-	for _, s := range service {
-		fmt.Println(s.Name)
-	}
-	fmt.Println("1111111")
 
 	return service, nil
 }
@@ -81,7 +70,6 @@ func (k *k8sSvcRegister) ListServices(...registry.ListOption) ([]*registry.Servi
 
 	var service []*registry.Service
 	var nodes []*registry.Node
-	//
 	ipMaps, err := getDnsForPodIP(k.k8sService)
 	if err != nil {
 		return []*registry.Service{}, err
@@ -94,15 +82,12 @@ func (k *k8sSvcRegister) ListServices(...registry.ListOption) ([]*registry.Servi
 		service = append(service, &registry.Service{Name: svcName, Version: "latest", Nodes: nodes})
 
 	}
-	fmt.Println("222222")
-
-	fmt.Println(service)
-	fmt.Println("2222")
 
 	return service, nil
 }
 
 func (k *k8sSvcRegister) Watch(option ...registry.WatchOption) (registry.Watcher, error) {
+	// 因为我们打算用k8s service的服务注册自己发现endpoints，所以我们自己并不需要写注册发现逻辑
 	return &k8sSvcWatcher{}, nil
 }
 func (k *k8sSvcRegister) String() string {
